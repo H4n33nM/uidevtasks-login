58de2a2cff79e9490af6ee90f35d399d9a666a04
Haneen <haninmohamed@graduate.utm.my>
2024-09-19 20:58:18 +0300

    Added login form

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,11 +1,24 @@
 <template>
-  <!--start here-->
-  <div class="hello">
-    hello!
-  </div>
+  <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+  <div class="login-container">
 
-  <div class="image">
-    <img :src="flowersImage" alt="Flowers" />
+    <!--login form-->
+    <div class="login-form">
+      <form @submit.prevent="handleLogin">
+        <p>Enter your email address</p>
+        <input v-model="email" type="email">
+        <p>Enter your password</p>
+        <input v-model="password" type="password">
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+
+    <!--image-->
+    <div class="image">
+      <img :src="flowersImage" alt="Flowers" />
+      <div class="overlay"></div>
+    </div>
+
   </div>
 </template>
 
@@ -15,20 +28,109 @@ export default {
   name: 'LoginPage',
   data() {
     return {
-      flowersImage: require('@/assets/flowers.png')
+      flowersImage: require('@/assets/flowers.png'),
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    handleLogin() {
+      if (this.email && this.password) {
+        this.$emit('login', { email: this.email, password: this.password });
+        this.email = '';
+        this.password = '';
+        alert('Login successful!');
+      }
     }
   }
 }
 </script>
 
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.login-container {
+  display: flex;
+  flex-direction: row-reverse;
+  width: 100vw;
+  height: 100vh;
+  font-family: 'Poppins';
+  font-size: 16px;
+  margin-top: 60px;
+}
+
 .image {
-  position: absolute;
+  display: flex;
+  flex: 1;
+}
+
+.image img {
+  position: relative;
+  right: 8px;
+  bottom: 60px;
   width: 720px;
   height: 1024px;
-  left: 0px;
+}
+
+.overlay {
+  position: absolute;
   top: 0px;
+  left: 0px;
+  width: 720px;
+  height: 1024px;
+  background-color: rgba(17, 17, 17, 0.25);
+  z-index: 1;
+}
+
+.login-form {
+  width: 50%;
+  padding: 20px;
+}
+
+p {
+  font-size: 16px;
+  color: #666666;
+  text-align: left;
+}
+
+input {
+  display: block;
+  border: 1px solid #66666659;
+  border-radius: 12px;
+  width: 568px;
+  height: 30px;
+  padding: 10px;
+  margin-bottom: 15px;
+  transition-duration: 0.3s;
+}
+
+input:hover {
+  border: 1px solid #997825;
+}
+
+button {
+  display: block;
+  font-family: 'Poppins';
+  font-size: 20px;
+  color: #ffffff;
+  background-color: #997825;
+  border: 1px solid #997825;
+  border-radius: 32px;
+  width: 304px;
+  height: 64px;
+  margin-top: 35px;
+  transition-duration: 0.3s;
+}
+
+button:hover {
+  display: block;
+  font-family: 'Poppins';
+  font-size: 20px;
+  color: #ffffff;
+  background-color: #65501a;
+  border: 1px solid #65501a;
+  border-radius: 32px;
+  width: 304px;
+  height: 64px;
+  margin-top: 35px;
 }
 </style>
