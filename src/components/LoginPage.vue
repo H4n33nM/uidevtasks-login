31638a70b0718bb303f52f3c91c05f2b9cea938a
Haneen <haninmohamed@graduate.utm.my>
2024-09-20 19:19:44 +0300

    Adjusted login form

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,16 +1,25 @@
 <template>
   <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-  <div class="login-container">
+  <link href='https://fonts.googleapis.com/css?family=Avenir' rel='stylesheet'>
 
+  <div class="login-container">
     <!--login form-->
     <div class="login-form">
       <form @submit.prevent="handleLogin">
-        <p>Enter your email address</p>
+        <h1>Welcome back!</h1>
+        <div class="alternate-login">
+          <button class="alt">Continue with Google</button>
+          <button class="alt">Continue with Twitter</button>
+        </div>
+        <hr class="divider">
+        Enter your email address
         <input v-model="email" type="email">
-        <p>Enter your password</p>
+        Enter your password
         <input v-model="password" type="password">
-        <button type="submit">Sign In</button>
+        <span class="hyperlink">Forgot your password?</span>
+        <button class="sign-in" type="submit">Sign In</button>
       </form>
+      <p>Don't have an account? <span class="hyperlink">Sign up here!</span></p>
     </div>
 
     <!--image-->
@@ -48,6 +57,11 @@ export default {
 
 
 <style scoped>
+h1 {
+  color: #333333;
+  font-size: 32px;
+}
+
 .login-container {
   display: flex;
   flex-direction: row-reverse;
@@ -56,8 +70,12 @@ export default {
   font-family: 'Poppins';
   font-size: 16px;
   margin-top: 60px;
+  font-size: 16px;
+  color: #666666;
+  text-align: left;
 }
 
+/*Image*/
 .image {
   display: flex;
   flex: 1;
@@ -81,15 +99,17 @@ export default {
   z-index: 1;
 }
 
+/*Login Form*/
+hr.divider {
+  border-top: 1px solid #66666640;
+  width: 590px;
+  position: absolute;
+  top: 365px;
+}
+
 .login-form {
   width: 50%;
   padding: 20px;
-}
-
-p {
-  font-size: 16px;
-  color: #666666;
-  text-align: left;
 }
 
 input {
@@ -107,7 +127,21 @@ input:hover {
   border: 1px solid #997825;
 }
 
-button {
+.hyperlink {
+  text-decoration: underline;
+  color: #111111;
+  text-align: center;
+}
+
+.hyperlink:hover {
+  color: #0000ff;
+}
+
+.hyperlink:active {
+  color: #ff0000;
+}
+
+.sign-in {
   display: block;
   font-family: 'Poppins';
   font-size: 20px;
@@ -121,7 +155,7 @@ button {
   transition-duration: 0.3s;
 }
 
-button:hover {
+.sign-in:hover {
   display: block;
   font-family: 'Poppins';
   font-size: 20px;
@@ -132,5 +166,36 @@ button:hover {
   width: 304px;
   height: 64px;
   margin-top: 35px;
+}
+
+.alt {
+  display: block;
+  font-family: 'Avenir';
+  font-size: 22px;
+  color: #333333;
+  background-color: #ffffff;
+  border: 1px solid #333333;
+  border-radius: 32px;
+  width: 568px;
+  height: 64px;
+  margin-top: 35px;
+  transition-duration: 0.3s;
+}
+
+.alt:hover {
+  display: block;
+  font-family: 'Avenir';
+  font-size: 22px;
+  color: #333333;
+  background-color: #e1e1e1;
+  border: 1px solid #333333;
+  border-radius: 32px;
+  width: 568px;
+  height: 64px;
+  margin-top: 35px;
+}
+
+.alternate-login {
+  padding: 0px 10px 40px 10px;
 }
 </style>
